b869742e1bdb4c1e51974a52d92c0917712770aa
johir-xs <Johir@xpeedstudio.com>
2021-08-23 13:28:53 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import { Button } from 'antd';
+import 'antd/dist/antd.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Button type="primary">Primary Button</Button>
+      <Button>Default Button</Button>
+      <Button type="dashed">Dashed Button</Button>
+      <br />
+      <Button type="text">Text Button</Button>
+      <Button type="link">Link Button</Button>
+    </div>
+  );
+}
+
+export default App;
